e4bea0d808697fc73a56c01333e4279bf0fdfa19
Serhat SARI <serhhatsari@gmail.com>
2025-06-28 14:10:32 +0300

    fix: wait for cert manager t o fully initilizae

--- k8s/certs/setup.sh
+++ k8s/certs/setup.sh
@@ -4,6 +4,8 @@ kubectl apply -f https://github.com/cert-manager/cert-manager/releases/latest/do
 
 kubectl apply -f cloudflare-api-token-secret.yml
 
+sleep 10
+
 kubectl apply -f cluster-issuer.yml
 
 kubectl apply -f certificate.yml
